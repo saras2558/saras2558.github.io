8cb22cf8a37cc975ec383eb1d6617c1fb8fb7419
saras2558 <73479762+saras2558@users.noreply.github.com>
2020-10-26 09:37:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Sara Saleh"
+let userName = "saras2558";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
